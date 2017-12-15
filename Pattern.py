fa85eb78871ef4e3548088f06a7eba6462d9d052
Alex Solomonov <solomonoff.alexandr@gmail.com>
2017-12-15 20:14:46 +0500

    Update

--- Pattern.py
+++ Pattern.py
@@ -11,64 +11,59 @@ import matplotlib.pyplot as plt
 
 import Parts
 
-# #
-w_0 = 3.*1e-03 #Gaussian beam size in m
-wavelenth = 555*1e-09 # in mb
-Topological_charge = 1
-Nodes_radial = 0 #Number of nodes in radial direction
+def Calculation_pattern(x_max, y_max, Resolution_x, Resolution_y, l, w_0):
+	# # Creation Calc Space
+	r, theta = Parts.Create_space.Generate(x_max, y_max, Resolution_x, Resolution_y)
+	
+	amplitude = np.zeros_like(r)
+	phase = np.zeros_like(r)
+	
+	amplitude, phase = Parts.eq.Bessel(r/w_0, theta, l)
+
+	new_phase = Parts.Code_ampl.Kotlyar(amplitude, phase)
+
+	# # amplitude
+	im = plt.matshow(amplitude**2, cmap = cm.gray)
+	plt.colorbar(im, orientation='horizontal')
+
+	sizes = np.shape(phase)
+	print(sizes)
+	fig = plt.figure()
+	fig.set_size_inches(1. * sizes[0] / sizes[1], 1, forward = False)
+	ax = plt.Axes(fig, [0., 0., 1., 1.])
+	ax.set_axis_off()
+	fig.add_axes(ax)
+	im = ax.imshow(phase, cmap=cm.gray)
+
+	sizes = np.shape(phase)
+	fig = plt.figure()
+	fig.set_size_inches(1. * sizes[0] / sizes[1], 1, forward = False)
+	ax = plt.Axes(fig, [0., 0., 1., 1.])
+	ax.set_axis_off()
+	fig.add_axes(ax)
+	im = ax.imshow(new_phase, cmap=cm.gray)
+	plt.savefig('phase.png', dpi = sizes[0], cmap=cm.gray, bbox_inches='tight')
+	# plt.savefig('phase.png', dpi = sizes[0], cmap=cm.gray, bbox_inches='tight')
 
 
+	plt.show()
+# # my_path = os.path.dirname(__file__)
+# # print(my_path)
 
-# # LC R1080
-Width = 10.56*1e-03 # im m
-Resolution = 1200 #pixels
+if __name__ == '__main__':
+	# # LC-R1080 parametrs
+	Width_x = 16.38*1e-03 # im m
+	Width_y = 10.56*1e-03 
+	Resolution_x = 1920 #pixels
+	Resolution_y = 1200 
 
+	# # Beam parametr
+	Topological_charge = 1
+	w_0 = 1.5*1e-03 #Gaussian beam size in m
 
-r, theta = Parts.Create_space.Generate(Width/2, Width/2, Resolution, Resolution)
-
-
-amplitude = np.zeros_like(r)
-phase = np.zeros_like(r)
-
-
-
-# amplitude, phase = Parts.eq.LG_modes(r = r, theta = theta, z=0,
-# 	l = Topological_charge,
-# 	p = Nodes_radial,
-# 	wavelenth = wavelenth,
-# 	w_0 = w_0)
-amplitude, phase = Parts.eq.F(r = r, theta = theta,
+	Calculation_pattern(Width_y/2,
+	Width_x/2,
+	Resolution_y,
+	Resolution_x,
 	l = Topological_charge,
 	w_0 = w_0)
-
-# E_0 = (1/np.max(Ampl))**2
-
-new_phase = Parts.Code_ampl.Kotlyar(amplitude, phase)
-
-# my_path = os.path.dirname(__file__)
-# print(my_path)
-
-# # amplitude
-fig, ax = plt.subplots(1,1)
-im = ax.imshow(amplitude**2, origin = 'lower', cmap=cm.gray)
-plt.colorbar(im, orientation='horizontal')
-
-# # phase
-fig, ax = plt.subplots(1,1)
-im = ax.imshow(phase, origin = 'lower', cmap=cm.gray)
-plt.axis('off')
-
-# # new_phase
-fig, ax = plt.subplots(1,1)
-im = ax.imshow(new_phase, origin = 'lower', cmap=cm.gray)
-plt.axis('off')
-
-# # # Diff
-# fig, ax = plt.subplots(1,1)
-# im = ax.imshow(new_phase-phase, origin = 'lower', cmap=cm.gray)
-# plt.axis('off')
-plt.show()
-
-
-# if __name__ == '__main__':
-# 	main()
