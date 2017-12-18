403adcc5b721eff5cfcf4b56c4a701ba4d19b73c
Alex Solomonov <solomonoff.alexandr@gmail.com>
2017-12-18 15:21:15 +0500

    Optim.

--- Parts/Code_ampl.py
+++ Parts/Code_ampl.py
@@ -4,17 +4,21 @@ def Kotlyar(amplitude, phase):
 	sizes = np.shape(phase)
 
 	new_phase = np.empty([2*sizes[0], 2*sizes[1]], float)
+	u = np.empty([sizes[0], sizes[1]], float)
+	v = np.empty([sizes[0], sizes[1]], float)
+	
+	u = phase + np.arccos(amplitude/2)
+	v = phase - np.arccos(amplitude/2)
+
 
 	for i in range(sizes[0]):
 		for j in range(sizes[1]):
-			new_phase[2*i, 2*j] = new_phase[2*i+1, 2*j+1] = phase[i,j] + np.arccos(amplitude[i,j]/2)
-			new_phase[2*i, 2*j+1] = new_phase[2*i+1, 2*j] = phase[i,j] - np.arccos(amplitude[i,j]/2)
+			new_phase[2*i, 2*j] = new_phase[2*i+1, 2*j+1] = u[i,j]
+			new_phase[2*i, 2*j+1] = new_phase[2*i+1, 2*j] = v[i,j]
 
-			for k in range(1):
-				for n in range(1):
-					if new_phase[2*i+k, 2*j+n] > np.pi:
-						new_phase[2*i+k, 2*j+n] = new_phase[2*i+k, 2*j+n] - 2*np.pi
-					if new_phase[2*i+k, 2*j+n] < -np.pi:
-						new_phase[2*i+k, 2*j+n] = new_phase[2*i+k, 2*j+n] + 2*np.pi
 
+	condlist = [new_phase <= np.pi , new_phase > np.pi, new_phase < -np.pi]
+	choicelist = [new_phase, new_phase-2*np.pi, new_phase+2*np.pi]
+
+	np.select(condlist, choicelist)
 	return new_phase
