4518b24eec67159de7385d74198cc74e2d3fb5cd
Alex Solomonov <solomonoff.alexandr@gmail.com>
2017-12-21 13:35:16 +0500

    Upd

--- Pattern.py
+++ Pattern.py
@@ -3,72 +3,42 @@ import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-import os
 import numpy as np
-import matplotlib.cm as cm
-import matplotlib.mlab as mlab
-import matplotlib.pyplot as plt
 
 import Parts
 
-# #
-w_0 = 3.*1e-03 #Gaussian beam size in m
-wavelenth = 555*1e-09 # in mb
-Topological_charge = 1
-Nodes_radial = 0 #Number of nodes in radial direction
+def Calculation_pattern(x_max, y_max, Resolution_x, Resolution_y, l, w_0):
+	# # Creation Calc Space
 
+	r, theta = Parts.Create_space.Generate(x_max, y_max, Resolution_x, Resolution_y)
+	
+	amplitude = np.zeros_like(r)
+	phase = np.zeros_like(r)
+	
+	amplitude, phase = Parts.eq.Bessel(r/w_0, theta, l)
+	amplitude = Parts.eq.Square(r)
 
+	new_phase = Parts.Code_ampl.Kotlyar(amplitude, phase)
 
-# # LC R1080
-Width = 10.56*1e-03 # im m
-Resolution = 1200 #pixels
+	# Parts.Image.show(amplitude**2)
+	# Parts.Image.show(phase)
+	Parts.Image.show(new_phase)
+	Parts.Image.save(new_phase, '../list_of_figs/sq_phase.png')
 
+if __name__ == '__main__':
+	# # LC-R1080 parametrs
+	Width_x = 16.38*1e-03 # im m
+	Width_y = 10.56*1e-03 
+	Resolution_x = 1920 #pixels
+	Resolution_y = 1200 
 
-r, theta = Parts.Create_space.Generate(Width/2, Width/2, Resolution, Resolution)
+	# # Beam parametr
+	Topological_charge = 1
+	w_0 = 1.5*1e-03 #Gaussian beam size in m
 
-
-amplitude = np.zeros_like(r)
-phase = np.zeros_like(r)
-
-
-
-# amplitude, phase = Parts.eq.LG_modes(r = r, theta = theta, z=0,
-# 	l = Topological_charge,
-# 	p = Nodes_radial,
-# 	wavelenth = wavelenth,
-# 	w_0 = w_0)
-amplitude, phase = Parts.eq.F(r = r, theta = theta,
+	Calculation_pattern(Width_y/2,
+	Width_x/2,
+	Resolution_y/2,
+	Resolution_x/2,
 	l = Topological_charge,
 	w_0 = w_0)
-
-# E_0 = (1/np.max(Ampl))**2
-
-new_phase = Parts.Code_ampl.Kotlyar(amplitude, phase)
-
-# my_path = os.path.dirname(__file__)
-# print(my_path)
-
-# # amplitude
-fig, ax = plt.subplots(1,1)
-im = ax.imshow(amplitude**2, origin = 'lower', cmap=cm.gray)
-plt.colorbar(im, orientation='horizontal')
-
-# # phase
-fig, ax = plt.subplots(1,1)
-im = ax.imshow(phase, origin = 'lower', cmap=cm.gray)
-plt.axis('off')
-
-# # new_phase
-fig, ax = plt.subplots(1,1)
-im = ax.imshow(new_phase, origin = 'lower', cmap=cm.gray)
-plt.axis('off')
-
-# # # Diff
-# fig, ax = plt.subplots(1,1)
-# im = ax.imshow(new_phase-phase, origin = 'lower', cmap=cm.gray)
-# plt.axis('off')
-plt.show()
-
-
-# if __name__ == '__main__':
-# 	main()
