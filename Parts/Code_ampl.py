dbb85419a549f4ecf6b37dca4ff5527a08b43332
Alex Solomonov <solomonoff.alexandr@gmail.com>
2017-12-21 13:32:42 +0500

    optimize code

--- Parts/Code_ampl.py
+++ Parts/Code_ampl.py
@@ -1,20 +1,24 @@
 import numpy as np
 
 def Kotlyar(amplitude, phase):
-	u = np.zeros_like(amplitude)
-	v = np.zeros_like(amplitude)
-	new_phase = np.zeros_like(amplitude)
+	sizes = np.shape(phase)
 
-	for i in range(len(amplitude)):
-		for j in range(len(amplitude)):
-			if j % 2 == 0:
-				u[i,j] = phase[i,j] + np.arccos(amplitude[i,j]/2)
-				v[i,j] = 0
+	new_phase = np.empty([2*sizes[0], 2*sizes[1]], float)
+	u = np.empty([sizes[0], sizes[1]], float)
+	v = np.empty([sizes[0], sizes[1]], float)
+	
+	u = phase + np.arccos(amplitude/2)
+	v = phase - np.arccos(amplitude/2)
 
-			else:
-				u[i,j] = 0
-				v[i,j] = phase[i,j] - np.arccos(amplitude[i,j]/2)
 
-	new_phase = u + v
+	for i in range(sizes[0]):
+		for j in range(sizes[1]):
+			new_phase[2*i, 2*j] = new_phase[2*i+1, 2*j+1] = u[i,j]
+			new_phase[2*i, 2*j+1] = new_phase[2*i+1, 2*j] = v[i,j]
 
+
+	condlist = [new_phase <= np.pi , new_phase > np.pi, new_phase < -np.pi]
+	choicelist = [new_phase, new_phase-2*np.pi, new_phase+2*np.pi]
+
+	np.select(condlist, choicelist)
 	return new_phase
