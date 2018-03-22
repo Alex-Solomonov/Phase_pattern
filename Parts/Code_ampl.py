95bbf13a6401724d08c834365656ac9127865a5e
Alex Solomonov <solomonoff.alexandr@gmail.com>
2018-03-23 01:24:40 +0500

    Added new pattern finding function.

--- Parts/Code_ampl.py
+++ Parts/Code_ampl.py
@@ -1,14 +1,16 @@
 import numpy as np
 import scipy.optimize
 import scipy.special
+import Parts
 
 
-def Kotlyar(amplitude, phase):
-	sizes = np.shape(phase)
+def Kotlyar(field):
+	# Init par's
+	sizes = np.shape(field)
+	amplitude = Parts.Misc.get_amplitude(field)
+	phase = Parts.Misc.get_phase(field)
 
-	new_phase = np.empty([2*sizes[0], 2*sizes[1]], float)
-	u = np.empty([sizes[0], sizes[1]], float)
-	v = np.empty([sizes[0], sizes[1]], float)
+	pattern = np.empty([2*sizes[0], 2*sizes[1]], float)
 	
 	u = phase + np.arccos(amplitude/2)
 	v = phase - np.arccos(amplitude/2)
@@ -16,71 +18,36 @@ def Kotlyar(amplitude, phase):
 
 	for i in range(sizes[0]):
 		for j in range(sizes[1]):
-			new_phase[2*i, 2*j] = new_phase[2*i+1, 2*j+1] = u[i,j]
-			new_phase[2*i, 2*j+1] = new_phase[2*i+1, 2*j] = v[i,j]
+			pattern[2*i, 2*j] = pattern[2*i+1, 2*j+1] = u[i,j]
+			pattern[2*i, 2*j+1] = pattern[2*i+1, 2*j] = v[i,j]
 
-	new_phase = np.angle(np.exp(1j*new_phase), deg=True)
-	return new_phase
-
-def sinc(x):
-	return np.sin(np.pi*x)/(np.pi*x)
+	pattern = np.angle(np.exp(1j*pattern), deg=True)
+	return pattern
 
 
+def Fuentes(field, LSM_pars):
+	# Init par's
+	sizes = np.shape(field)
+	amplitude = Parts.Misc.get_amplitude(field)
+	phase = Parts.Misc.get_phase(field)
 
-def Arrison_CGH1(amplitude, phase):
-	sizes = np.shape(phase)
-	encoded_amplitude = np.empty([sizes[0], sizes[1]], float)
-
-	def fun_CGH1(x, amplitude):
-		return sinc(1-x) - amplitude
+	random_mask = np.zeros_like(field)
 
 	for i in range(sizes[0]):
-		for  j in range(sizes[0]):
-			sol = scipy.optimize.minimize(lambda x: fun_CGH1(x, amplitude[i,j]), x0 = 1e-03)
-			encoded_amplitude[i, j] = sol.x[0]
+		for j in range(sizes[1]):
+			# rdn is selected as uniformly distributed in the range [0;1)
+			rnd = np.random.rand(1,1)
+			if amplitude[i,j] > rnd:
+				random_mask[i,j] = 1
 
-	new_phase = encoded_amplitude*phase
-	new_phase = np.angle(np.exp(1j*new_phase), deg=True)
-	return new_phase
+	noise_lvl = np.zeros_like(field)
 
+	noise_like_signal = random_mask - amplitude
 
-def Arrison_CGH2(amplitude, phase):
-	sizes = np.shape(phase)
-	encoded_amplitude = np.empty([sizes[0], sizes[1]], float)
+	diverging_element = Parts.eq.axicon(LSM_pars, cycle = 160)
 
-	def fun_CGH2(x, amplitude):
-		return scipy.special.jv(0, x) - amplitude
+	pattern = field \
+		+ ((1-amplitude) - noise_like_signal) * diverging_element \
+		+ noise_like_signal*np.exp(1j*phase)
 
-
-	for i in range(sizes[0]):
-		for  j in range(sizes[0]):
-			sol = scipy.optimize.minimize(lambda x: fun_CGH2(x, amplitude[i,j]), x0 = 1e-03)
-			encoded_amplitude[i, j] = sol.x[0]
-
-	new_phase = phase + encoded_amplitude*np.sin(phase)
-
-	new_phase = np.angle(np.exp(1j*new_phase), deg=True)
-	return new_phase
-
-def Arrison_CGH3(amplitude, phase):
-	sizes = np.shape(phase)
-	encoded_amplitude = np.empty([sizes[0], sizes[1]], float)
-
-	def fun_CGH3(x, amplitude):
-		return scipy.special.jv(1, x) - amplitude
-
-
-	for i in range(sizes[0]):
-		for  j in range(sizes[0]):
-			sol = scipy.optimize.minimize(lambda x: fun_CGH3(x, amplitude[i,j]), x0 = 1e-03)
-			encoded_amplitude[i, j] = sol.x[0]
-
-	new_phase = encoded_amplitude*np.sin(phase)
-
-	new_phase = np.angle(np.exp(1j*new_phase), deg=True)
-	return new_phase
-
-def Arrison_Phase_carrier(amplitude, phase):
-	new_phase = 0
-	new_phase = np.angle(np.exp(1j*new_phase), deg=True)
-	return new_phase
+	return pattern
